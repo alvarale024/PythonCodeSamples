c283d34f3a26815543d9b0999eda1b92a808e51e
alvarale024 <132589850+alvarale024@users.noreply.github.com>
2023-05-31 23:07:43 -0500

    Add files via upload

---
+++ m3p5.py
@@ -0,0 +1,12 @@
+#Alejandro Alvarez
+#5/14/2023
+#The program converts fahrenheit to celsius
+
+#The users temperature input in fahrenheit
+fahrenheit = float(input("Enter the temperature in Fahrenheit: "))
+
+#Fahrenheit to celsius converstion calculation 
+celsius = (fahrenheit - 32) * 5/9
+
+#Converted tempature in celsius
+print("The temperature in Celsius is:", celsius)
